b1534733931710b299af91b26a09e10620ae3984
AlexPrograms <olexiy2004@gmail.com>
2023-12-12 22:05:43 +0100

    Adding State pattern

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,23 +9,110 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private SpriteRenderer sprite;
     private float dirX = 0f;
-    [SerializeField]private float jumpForce = 14f;
-    [SerializeField]private float runSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float runSpeed = 7f;
     [SerializeField] private LayerMask jumpableGround;
-    private enum movementState { idle, running, jumping, falling };
-
     [SerializeField] private AudioSource jumpSoundEffect;
-    
+
+    private IPlayerState currentState;
+
+    public enum MovementState
+    {
+        Idle,
+        Running,
+        Jumping,
+        Falling
+    }
+
+    public interface IPlayerState
+    {
+        void EnterState(PlayerMovement player);
+        void UpdateState(PlayerMovement player);
+    }
+
+    public class IdleState : IPlayerState
+    {
+        public void EnterState(PlayerMovement player) => player.SetAnimationState(MovementState.Idle);
+        public void UpdateState(PlayerMovement player)
+        {
+            if (Mathf.Abs(player.dirX) > 0)
+                player.SetState(new RunningState());
+        }
+    }
+
+    public class RunningState : IPlayerState
+    {
+        public void EnterState(PlayerMovement player)
+        {
+            player.SetAnimationState(MovementState.Running);
+            player.UpdateSpriteDirection();
+        }
+
+        public void UpdateState(PlayerMovement player)
+        {
+            if (Mathf.Abs(player.dirX) == 0)
+                player.SetState(new IdleState());
+            else
+                player.UpdateSpriteDirection();
+        }
+    }
+
+    public class JumpingState : IPlayerState
+    {
+        private bool hasFlipped = false;
+
+        public void EnterState(PlayerMovement player)
+        {
+            player.SetAnimationState(MovementState.Jumping);
+            player.UpdateSpriteDirection();
+            hasFlipped = false;
+        }
+
+        public void UpdateState(PlayerMovement player)
+        {
+            player.UpdateSpriteDirection();
+
+            if (player.rb.velocity.y < 0.1f)
+            {
+                player.SetState(new FallingState());
+                hasFlipped = false; 
+            }
+
+
+            if (!hasFlipped && Mathf.Abs(player.dirX) > 0)
+            {
+                player.sprite.flipX = player.dirX < 0; 
+                hasFlipped = true; 
+            }
+        }
+    }
+
+    public class FallingState : IPlayerState
+    {
+        public void EnterState(PlayerMovement player)
+        {
+            player.UpdateSpriteDirection();
+            player.SetAnimationState(MovementState.Falling);
+        }
+
+        public void UpdateState(PlayerMovement player)
+        {
+            player.UpdateSpriteDirection();
+            if (player.IsGrounded())
+                player.SetState(new IdleState());
+        }
+    }
+
     private void Start()
     {
-        Debug.Log("Hello world");
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         bColl = GetComponent<BoxCollider2D>();
+
+        SetState(new IdleState());
     }
 
-    
     private void Update()
     {
         dirX = Input.GetAxisRaw("Horizontal");
@@ -35,45 +122,31 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpSoundEffect.Play();
+            SetState(new JumpingState());
         }
 
-        UpdateAnimationState();
+        currentState.UpdateState(this);
     }
 
-    private void UpdateAnimationState()
+    public void SetState(IPlayerState state)
     {
+        currentState = state;
+        currentState.EnterState(this);
+    }
 
-        movementState state;
-
-        if (dirX > 0f)
-        {
-            state = movementState.running;
-            sprite.flipX = false;
-        }
-        else if (dirX < 0f)
-        {
-            state = movementState.running;
-            sprite.flipX = true;
-        }
-        else
-        {
-            state = movementState.idle;
-        }
-
-        if (rb.velocity.y > .1f) 
-        { 
-            state = movementState.jumping;
-        }
-        else if (rb.velocity.y < -.1f)
-        {
-            state = movementState.falling;
-        }
-
+    public void SetAnimationState(MovementState state)
+    {
         anim.SetInteger("state", (int)state);
+    }
+
+    public void UpdateSpriteDirection()
+    {
+        if (Mathf.Abs(dirX) > 0)
+            sprite.flipX = dirX < 0;
     }
 
     private bool IsGrounded()
     {
-       return Physics2D.BoxCast(bColl.bounds.center, bColl.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        return Physics2D.BoxCast(bColl.bounds.center, bColl.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
